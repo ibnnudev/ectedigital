d2d4ec9867fa616bf328a9ba85204406b1452cbe
Moh Ibnu Abdurrohman Sutio <ibnuabdurrohmansutio@gmail.com>
2024-02-07 17:44:58 +0700

    Update fonts and add Flowbite CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,7 @@ export default {
     theme: {
         extend: {
             fontFamily: {
-                sans: ["Figtree", ...defaultTheme.fontFamily.sans],
+                sans: ["Inter", "Sora", ...defaultTheme.fontFamily.sans],
             },
             colors: {
                 primary: "#1BC35E",
