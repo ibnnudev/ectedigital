b567dd5e756994a7eb1f4d3fc65f46503492262f
Moh Ibnu Abdurrohman Sutio <ibnuabdurrohmansutio@gmail.com>
2024-02-08 13:02:45 +0700

    Add portfolio detail page and route

---
+++ resources/views/guest/portfolio_detail.blade.php
@@ -0,0 +1,217 @@
+<x-guest-layout>
+    {{-- Hero --}}
+    <section class="bg-primary relative">
+        <div class="relative z-10">
+            <div class="items-center px-8 pt-12 pb-20 mx-auto max-w-7xl lg:pt-14 grid grid-cols-12 gap-x-14 mb-16">
+                <div class="col-span-6">
+                    <h1 class="text-5xl font-bold tracking-tight text-white sora leading-snug capitalize max-w-lg">
+                        Point Of Sale: Dapur mami
+                    </h1>
+                    <p class="text-white leading-7 max-w-md mt-7">
+                        Dapur mami merupakan sebuah restoran/kafe di daerah Situbondo yang menawarkan layanan pemesanan
+                        makanan dan reservasi tempat untuk kegiatan atau event. Kami berusaha mewujudkan kebutuhan
+                        tersebut
+                    </p>
+                    <a href="#_"
+                        class="mt-10 items-center justify-center w-full px-7 py-3 text-center text-primary duration-200 bg-white border border-white rounded-full inline-flex hover:bg-transparent hover:border-white hover:bg-white hover:text-white focus:outline-none lg:w-auto focus-visible:outline-white shadow-btn">
+                        Selengkapnya
+                    </a>
+                </div>
+                <div class="col-span-6">
+                    <div class="rounded-xl bg-white px-6 py-8">
+                        <h1 class="text-black sora font-semibold">
+                            Informasi Proyek
+                        </h1>
+
+                        <div class="mt-10">
+                            <div class="grid grid-cols-2 items-center mb-3">
+                                <span class="font-medium">Kategori</span>
+                                <span class="text-[#5F6368]">Web Development</span>
+                            </div>
+                            <div class="grid grid-cols-2 items-center mb-3">
+                                <span class="font-medium">Client</span>
+                                <span class="text-[#5F6368]">PT. Aksara Media Sejahtera</span>
+                            </div>
+                            <div class="grid grid-cols-2 items-center mb-3">
+                                <span class="font-medium">Lokasi</span>
+                                <span class="text-[#5F6368]">Jember</span>
+                            </div>
+                            <div class="grid grid-cols-2 items-center mb-3">
+                                <span class="font-medium">Proyek Selesai</span>
+                                <span class="text-[#5F6368]">27 Januari 2024</span>
+                            </div>
+                            <div class="grid grid-cols-2 items-center mb-3">
+                                <span class="font-medium">Manajer</span>
+                                <span class="text-[#5F6368]">PT. Vokasi Indonesia Sejahtera</span>
+                            </div>
+                        </div>
+
+                        <a href="#_"
+                            class="mt-16 items-center justify-center w-full px-7 py-3 text-center text-white duration-200 bg-primary border border-primary rounded-full inline-flex hover:bg-transparent hover:border-primary hover:bg-primary hover:text-primary focus:outline-none focus-visible:outline-primary shadow-btn">
+                            Konsultasi Gratis!
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <img src="{{ asset('assets/pattern.png') }}" alt="" class="absolute top-2/3 left-0 right-0 z-0 mx-auto"
+            width="1100">
+    </section>
+    {{-- End Hero --}}
+
+    {{-- Problem and Solutions --}}
+    <section class="bg-white">
+        <div class="relative items-center w-full px-5 pt-12 mx-auto md:px-12 lg:px-16 max-w-7xl lg:py-32">
+            <div class="grid grid-cols-4 gap-6">
+                <div>
+                    <h1 class="font-semibold text-black text-2xl sora">😓 Masalah</h1>
+                    <p class="leading-6 text-gray-400 mt-6">
+                        Chubbydut memiliki sosial media dan toko di marketplace, pada suatu hari sosial media untuk
+                        jualan tidak dapat di akses sehingga tidak dapat melakuan promosi.
+                    </p>
+                </div>
+                <div>
+                    <h1 class="font-semibold text-black text-2xl sora">🤓 Solusi</h1>
+                    <p class="leading-6 text-gray-400 mt-6">
+                        Membuatkan toko online sebagai media promosi dan menjual produk-produknya dengan terintegrasi
+                        jasa pengiriman dan whatsapp serta Instagram Shop.
+                    </p>
+                </div>
+                <div>
+                    <h1 class="font-semibold text-black text-2xl sora">🪖 Hasil Kerja</h1>
+                    <ul class="list-disc leading-6 text-gray-400 mt-6 list-inside">
+                        <li>UI / UX Design</li>
+                        <li>Website</li>
+                        <li>Cloud Hosting</li>
+                        <li>Support and Maintenance</li>
+                    </ul>
+                </div>
+                <div>
+                    <h1 class="font-semibold text-black text-2xl sora">👨‍💻 Teknologi</h1>
+                    <ul class="list-disc leading-6 text-gray-400 mt-6 list-inside">
+                        <li>HTML, CSS, JS</li>
+                        <li>PHP, Laravel</li>
+                        <li>MySQL</li>
+                        <li>Cloudflare</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+    {{-- End Problem and Solutions --}}
+
+    {{-- Description --}}
+    <section class="bg-white">
+        <div class="relative items-center w-full px-5 pt-12 mx-auto md:px-12 lg:px-16 max-w-7xl lg:py-32">
+            <div class="grid grid-cols-12 gap-6">
+                <div class="col-span-6">
+                    <div class="text-black bg-[#F2F2F2] px-3 py-2 text-sm w-fit rounded-full mb-6">
+                        Ringkasan
+                    </div>
+                    <h1 class="font-semibold text-4xl leading-snug mb-9 sora">
+                        B2B Marketing Agency Website Development
+                    </h1>
+                    <p class="leading-6 text-desc-font">
+                        KKBC adalah agensi B2B Jepang yang menggunakan dan memaksimalkan data untuk memahami perilaku
+                        manusia, menciptakan strategi yang sukses, dan mengubah investasi pemasaran menjadi keuntungan
+                        pemasaran. KKBC menyediakan layanan dwi-bahasa yang memungkinkan kami melakukan yang terbaik
+                        dalam bidang-bidang spesifik dari kebutuhan klien kami. KKBC berfokus pada memaksimalkan nilai
+                        perusahaan multinasional dan merek produk di Jepang.
+                    </p>
+                    <p class="leading-6 text-desc-font mt-6">
+                        Kemampuan KKBC meliputi media, pemasaran, pemasaran digital, hubungan masyarakat dan pers,
+                        kreatif, dan promosi - memberikan solusi terpadu untuk manajemen komunikasi. Tim ahli strategi
+                        dan perencana multikultural KKBC di Jepang memberikan hasil yang hemat biaya, dengan kecepatan
+                        bisnis global, untuk mengembangkan program terpadu yang memisahkan klien kami dari pesaing
+                        mereka.
+                    </p>
+                    <a href="#_"
+                        class="mt-14 items-center justify-center w-auto px-7 py-3 text-center text-white duration-200 bg-primary border border-primary rounded-full inline-flex hover:bg-transparent hover:border-primary hover:bg-primary hover:text-primary focus:outline-none focus-visible:outline-primary shadow-btn">
+                        Kunjungi Proyek
+                    </a>
+                </div>
+                <div></div>
+                <div class="col-span-5">
+                    <img src="{{ asset('assets/bgdetaliportfolio.png') }}"
+                        class="object-cover object-center rounded-3xl w-full" height="419" alt="">
+                </div>
+            </div>
+        </div>
+    </section>
+    {{-- End Description --}}
+
+    {{-- FAQ --}}
+    {{-- FAQ --}}
+    <section class="bg-white">
+        <div class="relative items-center w-full px-5 pt-12 mx-auto md:px-12 lg:px-16 max-w-7xl lg:py-32">
+            <div class="mx-auto max-w-xl text-center mb-16">
+                <h3 class="font-semibold text-primary text-xl mb-8">FAQs</h3>
+                <h1 class="sora text-black font-semibold text-3xl mb-4">
+                    Pertanyaan yang Sering Diajukan
+                </h1>
+                <p class="text-desc-font leading-6">
+                    Berkembang bersama untuk wujudkan bisnis yang cemerlang. Gapai mimpimu untuk pemasaran produk yang
+                    lebih
+                    baik
+                </p>
+            </div>
+            <div class="grid grid-cols-2 gap-4">
+                <div class="active-faq">
+                    <h1 class="font-semibold text-xl mb-5">
+                        Bagaimana cara membeli produk?
+                    </h1>
+                    <p class="leading-6">
+                        Over 350,000 online businesses and entrepreneurs have used Modalyst. It's the easiest way to
+                        find the best print on demand companies
+                    </p>
+                </div>
+                <div class="faq">
+                    <h1 class="font-semibold text-xl mb-5">
+                        Apakah bisa di kostumisasi?
+                    </h1>
+                    <p class="leading-6">
+                        Over 350,000 online businesses and entrepreneurs have used Modalyst. It's the easiest way to
+                        find the best print on demand companies
+                    </p>
+                </div>
+                <div class="faq">
+                    <h1 class="font-semibold text-xl mb-5">
+                        Apakah ada garansi?
+                    </h1>
+                    <p class="leading-6">
+                        Over 350,000 online businesses and entrepreneurs have used Modalyst. It's the easiest way to
+                        find the best print on demand companies
+                    </p>
+                </div>
+                <div class="faq">
+                    <h1 class="font-semibold text-xl mb-5">
+                        Bagaimana dengan keamanan-nya?
+                    </h1>
+                    <p class="leading-6">
+                        Over 350,000 online businesses and entrepreneurs have used Modalyst. It's the easiest way to
+                        find the best print on demand companies
+                    </p>
+                </div>
+                <div class="faq">
+                    <h1 class="font-semibold text-xl mb-5">
+                        Apakah ada biaya tambahan?
+                    </h1>
+                    <p class="leading-6">
+                        Over 350,000 online businesses and entrepreneurs have used Modalyst. It's the easiest way to
+                        find the best print on demand companies
+                    </p>
+                </div>
+                <div class="faq">
+                    <h1 class="font-semibold text-xl mb-5">
+                        Apakah ada maintenance?
+                    </h1>
+                    <p class="leading-6">
+                        Over 350,000 online businesses and entrepreneurs have used Modalyst. It's the easiest way to
+                        find the best print on demand companies
+                    </p>
+                </div>
+            </div>
+        </div>
+    </section>
+    {{-- End FAQ --}}
+</x-guest-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,13 @@
 <?php
 
 use App\Http\Controllers\GuestController;
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [GuestController::class, 'index'])->name('guest.home');
 Route::get('about', [GuestController::class, 'about'])->name('guest.about');
 Route::get('service', [GuestController::class, 'service'])->name('guest.service');
 Route::get('portfolio', [GuestController::class, 'portfolio'])->name('guest.portfolio');
+Route::get('portfolio/{id}', [GuestController::class, 'portfolioDetail'])->name('guest.portfolio.detail');
 Route::get('contact', [GuestController::class, 'contact'])->name('guest.contact');
 
 require __DIR__ . '/auth.php';
